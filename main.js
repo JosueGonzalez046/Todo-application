871e73f3191b0de92d170ac28955f642bf25b51c
Josue Gonzalez <josuegonzalezd046@gmail.com>
2024-10-07 17:22:11 -0700

    updates

--- main.js
+++ main.js
@@ -12,8 +12,33 @@ if(taskText.trim() !== ''){ // makes sure its not an empty string if its not it 
     const newTask = document.createElement('li')
     newTask.textContent = taskText.toUpperCase()
 
+    //create a delete button
+    const deleteBtn = document.createElement('button')
+    deleteBtn.textContent = "Delete"
+    deleteBtn.classList.add('deleteBtn')
+    // add a checked button
+    const completedBtn = document.createElement('button')
+    completedBtn.textContent = "Completed"
+    completedBtn.classList.add('completedBtn')
+
+    //append delete button to task item
+    newTask.appendChild(deleteBtn)
+    //append completed button to task item
+    newTask.appendChild(completedBtn)
+
     // this appends the task to the new task list
     taskList.appendChild(newTask)
+
+    // adding event listener to task list using event delegation
+    taskList.addEventListener('click', function(e)  {
+        // checking if the elment clicked is a delete button or completed button
+        if(e.target.classList.contains('deleteBtn')) {
+            //remove the task from list
+            e.target.parentElement.remove()
+        }else if(e.target.classList.contains('completedBtn')) {
+            taskList.style.textDecoration = 'line-through'
+        }
+    })
 
     //clears the input after adding a new task
     taskInput.value = ''
@@ -23,3 +48,4 @@ if(taskText.trim() !== ''){ // makes sure its not an empty string if its not it 
     alert('Please enter a valid task!')
 }
 })
+
